ddeed8088c6c76bff507693b25b0a058e8fe60c6
Matthew Ghannoum <mattghanno@gmail.com>
2021-03-03 15:46:32 +1100

    Implemented basic draft student table

--- rds/devDB.sql
+++ rds/devDB.sql
@@ -3,4 +3,8 @@ create database courseRecommender_dev;
 create table student (
   fName varchar(50),
   sName varchar(50),
+  age TINYINT(1),
+  atar FLOAT(2, 2),
+  fullTimePreferred boolean,
+  coursesTaken mediumtext
 );
