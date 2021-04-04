60f040240f93df66034d7cd99be1651d1e511056
LGXprod <mattghanno@gmail.com>
2021-04-04 12:52:41 +1000

    Finished extracted and cleaning course, degree, major and subject data

---
+++ scripts/writeSQLInserts.js
@@ -0,0 +1,5 @@
+const courseAreasDegrees = require("./scraping/data/courseAreaDegrees.json");
+const collatedData = require("./scraping/data/collatedData.json");
+const subjectDetails = require("./scraping/data/subjectDetails.json");
+
+console.log(subjectDetails);
