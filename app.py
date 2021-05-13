725b7fcfae6c53d10113021da5ae3695fd28a846
Matthew Ghannoum <mattghanno@gmail.com>
2021-05-13 23:02:14 +1000

    test

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ import KNN
 app = Flask(__name__)
 
 config = dotenv_values(".env")
+print("config", config)
 
-connection = pymysql.connect(
-  host = config["HOST"], 
-  user = config["DB_USERNAME"],
-  password = config["DB_PASSWORD"], 
-  database = "crDB",
-  charset = 'utf8mb4',
-  cursorclass = pymysql.cursors.DictCursor
-)
+# connection = pymysql.connect(
+#   host = config["HOST"], 
+#   user = config["DB_USERNAME"],
+#   password = config["DB_PASSWORD"], 
+#   database = "crDB",
+#   charset = 'utf8mb4',
+#   cursorclass = pymysql.cursors.DictCursor
+# )
 
 data = KNN.curate("./sampledata.csv")
 # sample = {'Group Assignments': 0, 'Assignment Types': ['Quiz/test'], 'Keywords':['LTE', 'Prototyping']}
@@ -41,13 +42,13 @@ def postRecommendation():
       prediction = KNN.Prediction(quizData, data)
       similarity_list = prediction.get_similaritylist()
 
-      query = "update students set recommendations = %s where student_id = (select user_id as student_id from sessions where session_id=%s)"
-      vals = (json.dumps(similarity_list), body["session_id"])
+      # query = "update students set recommendations = %s where student_id = (select user_id as student_id from sessions where session_id=%s)"
+      # vals = (json.dumps(similarity_list), body["session_id"])
 
-      with connection.cursor() as cursor:
-        cursor.execute(query, vals)
+      # with connection.cursor() as cursor:
+      #   cursor.execute(query, vals)
 
-      connection.commit()
+      # connection.commit()
 
       return {
         "headers": {
@@ -68,5 +69,5 @@ def postRecommendation():
         "statusCode": 400
       }
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__app__':
+    app.run(debug=True)
